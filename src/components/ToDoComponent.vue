86157ece0c7a8d391749fcc7a0df734da2b57b8f
Alexey Sinyavskiy <sinyavskij_00@mail.ru>
2018-09-19 13:14:08 +0300

    last

--- src/components/ToDoComponent.vue
+++ src/components/ToDoComponent.vue
@@ -3,15 +3,15 @@
       <br>
     <div class="text-center">
       <button class="btn btn-danger" @click="clear">Clean All</button>
-              <input id="taskName" class="form-control" type="text" placeholder="Name...">
+              <input id="taskName" class="form-control w-50 centerPos" type="text" placeholder="Name...">
               <textarea id="taskInput" class="form-control w-50 centerPos" v-bind:placeholder="info"></textarea>
               <hr>
-              <button class="btn btn-secondary btn-xl" @click="createSome()">
+              <button class="btn btn-secondary btn-xl" @click="createSome">
               <i class="fa fa-plus"></i>
               </button>
               <br><br>
               <div class="list-group" v-if="view" v-for="(task,i) in tasks">
-                <div id="listPart" class="card" v-if="task.displays" @click="change">
+                <div id="listPart" class="card" v-if="task.displays">
                   <div class="card-header text-left">
                     <table>
                       <tr>
@@ -21,9 +21,32 @@
                      </td>
                      <td style="margin-left: 150px">
                        <span class="taskNameField">{{ task.name }}</span>
-                       </td>
+                      </td>
                      </tr>
                      </table>
+                    <button class="btn btn-success" data-toggle="modal" :data-target="'#demo' + task.ind" style="margin-left:250px;">
+                      <i class="fa fa-edit"></i>
+                    </button>
+<div class="modal fade" :id="'demo' + task.ind" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-edit"></i> Edit task</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <input id="taskName" class="form-control" type="text" :value="task.name" v-on:keyup="task.name = $event.target.value" placeholder="Name...">
+        <br>
+        <textarea id="taskInput" class="form-control w-50 centerPos" :value="task.text" v-on:keyup="task.text = $event.target.value" v-bind:placeholder="info"></textarea>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" @click="edit(task.ind, task.name, task.text)" data-dismiss="modal">Ok</button>
+      </div>
+    </div>
+  </div>
+</div>
                      <button class="btn btn-danger" @click="dropSome(task.ind); task.displays = false;" style="margin-left:200px;">
                       <i class="fa fa-times"></i>
                     </button>
@@ -47,7 +70,7 @@ export default {
     return {
       tasks: [
         {
-          ind: 1,
+          ind: 0,
           name: "",
           text: "",
           time: "",
@@ -61,17 +84,15 @@ export default {
   },
   created() {
     var indexes = [];
-    if (localStorage.length > 1) {
-      for (var s = 1; s < localStorage.length; s++) {
-        if (localStorage[s]) {
+      for (var s = 0; s < localStorage.length; s++) {
+        if (typeof localStorage[s] != 'undefined') {
           indexes.push(JSON.parse(localStorage[s]).ind);
         }
       }
-    }
     var elems = [];
-    for (var i = 1; i < localStorage.length; i++) {
+    for (var i = 0; i < localStorage.length; i++) {
       for (var j = 0; j < indexes.length; j++) {
-        if (localStorage[i]) {
+        if (typeof localStorage[i] != 'undefined') {
           if (JSON.parse(localStorage[i]).ind == indexes[j]) {
             elems.push(JSON.parse(localStorage[i]));
           }
@@ -80,8 +101,16 @@ export default {
     }
     this.tasks = elems;
     this.view = true;
+    this.fresh();
   },
   methods: {
+    edit (ind,name,text) {
+         var local = JSON.parse(localStorage.getItem(ind));
+         local.name = name;
+         local.text = text;
+         localStorage.removeItem(ind);
+         localStorage.setItem(ind, JSON.stringify(local));
+    },
     fresh() {
       if (localStorage.getItem("fresh")) {
         localStorage.removeItem("fresh");
@@ -123,7 +152,7 @@ export default {
         text: Ttext,
         time: Ttime,
         displays: Tdisplays
-      }
+      };
       this.tasks.push(objSave);
       localStorage.setItem(Tind, JSON.stringify(objSave));
       this.$noty.success("Task added successfully!");
@@ -150,8 +179,8 @@ export default {
       var name_inp = document.getElementById("taskName");
       if (inp.value == "" && name_inp.value == "") {
         this.warning("Nothing to record", inp);
-      } else { 
-        this.isOldTask(inp, name_inp);     
+      } else {
+        this.isOldTask(inp, name_inp);
         if (!this.err) {
           this.rememberTask(name_inp.value, inp.value, this.timeFormat());
           inp.value = "";
@@ -163,6 +192,8 @@ export default {
     dropSome(task_ind) {
       localStorage.removeItem(task_ind);
       this.$noty.success("Task has been removed!");
+      this.fresh();
+      throw true;
     },
     clear() {
       var check;
@@ -179,15 +210,12 @@ export default {
       setTimeout(function() {
         self.tasks = [];
         localStorage.clear();
-      }, 1000);
+      }, 500);
       if (check) {
         this.$noty.success("All tasks removed successfully!");
       } else {
         this.$noty.warning("Nothing to delete");
       }
-    },
-    change() {
-// Last feature
     }
   }
 };
@@ -228,10 +256,6 @@ export default {
 .card {
   width: 500px !important;
   margin-left: 432px;
-}
-
-.card:hover{
-  cursor: pointer; 
 }
 
 .delete {
